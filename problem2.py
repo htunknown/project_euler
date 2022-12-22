81f82fbf4fc90b5657d3d08de66ec74ff262475c
Mert OLÇAMAN <74461342+htunknown@users.noreply.github.com>
2022-12-22 22:13:28 +0300

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,17 @@
+def fibonacci_sequence(n):
+    """
+    Gives the "n" elements of fibonacci sequence
+    
+    >>fibonacci_sequence(10)
+    1,2,,5,8,13,21,34,55,89
+    """
+    a=0
+    b=1
+    m=[]
+    while len(m)<n:
+        b,a=(a+b),b
+        m.append(b)
+    return print(m)
+        
+        
+fibonacci_sequence(10)
