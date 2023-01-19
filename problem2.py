e77bb5991bb802fb1af59590358fb35627da9e05
Mert OLÇAMAN <74461342+htunknown@users.noreply.github.com>
2023-01-19 21:30:22 +0300

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -3,7 +3,7 @@ def fibonacci_sequence(n):
     Gives the "n" elements of fibonacci sequence
     
     >>fibonacci_sequence(10)
-    1,2,,5,8,13,21,34,55,89
+    1,2,3,5,8,13,21,34,55,89
     """
     a=0
     b=1
